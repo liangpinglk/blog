3b2e90b80053f1617359f547c5cbd83554610892
liangping <liangpinglk@gmail.com>
2020-08-14 22:21:51 +0800

    feat: add demo

---
+++ demo/frontend_download/excel_demo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <demo msg="Excel Dwonload Demo"/>
+  </div>
+</template>
+
+<script>
+import demo from './components/demo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    demo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
